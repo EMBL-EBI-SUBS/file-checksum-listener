0713ae33a04dd8d960877bc69012785ef78842e6
Karoly Erdos <karoly@ebi.ac.uk>
2019-02-19 10:54:14 +0000

    Fix configurations

--- src/main/java/uk/ac/ebi/subs/fileupload/fileprocessinglistener/config/FileCheckSumCalculatorConfig.java
+++ src/main/java/uk/ac/ebi/subs/fileupload/fileprocessinglistener/config/FileCheckSumCalculatorConfig.java
@@ -23,7 +23,7 @@ public class FileCheckSumCalculatorConfig {
     @Value("${fileProcessing-listener.checksum-calculator.jobName}")
     private String jobName;
 
-    @Value("#{environment.SPRING_PROFILE")
+    @Value("#{environment.SPRING_PROFILE}")
     private String profile;
 
     @Value("${fileProcessing-listener.configLocation}")
